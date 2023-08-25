5125f2cd35d01b517afd40c4d61f648c508c2229
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-08-25 17:36:39 +0100

    step 1 done

--- project.js
+++ project.js
@@ -9,15 +9,22 @@
 const prompt = require("prompt-sync")(); 
 
 const deposit = () => { 
-    const depositAmount = prompt("Enter a deposit amount: "); //simply telling user to enter amount 
-    const numberDepositAmount = parseFloat(depositAmount);  //making sure output is what user puts and no strings allowed
+    while (true) {  //This While statement makes it so the request is repeated constantly 
+     const depositAmount = prompt("Enter a deposit amount: "); //simply telling user to enter amount 
+     const numberDepositAmount = parseFloat(depositAmount);  //making sure output is what user puts and no strings allowed
 
-    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {  //NaN = Not a number 
+     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {  //NaN = Not a number 
         console.log("Invalid deposit amount try again.")
-    }
+     } else { 
+        return numberDepositAmount; 
+     }
+    }; 
 };
 
-deposit(); 
+const depositAmount = deposit(); 
+console.log(depositAmount)
+
+
 
 
 
